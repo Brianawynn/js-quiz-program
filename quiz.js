efb8eceff5a63711dada1a46845dcf9c839d79e5
Brianawynn <wynn.briana@wustl.edu>
2016-04-13 18:06:03 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,12 +1,22 @@
+document.write("<h1> A TEST ABOUT CATS</h1>")
 // variables
 var correct = 0;
 
 // prompt
-var answer1 = prompt("ask a question...");
-
+var ans1 = prompt("What do you call a group of cats?");
+var ans2 = prompt("How many muscles control a cat's ears?")
+var ans3 = prompt("Which state has elected a cat for mayor of one of its cities?")
+var ans4 = prompt("")
+var ans5 = prompt("")
 // if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
- correct += 1;
+if ( ans1.toUpperCase() === 'CLOWDER' ) {
+ correct = correct+ 1;
+}
+if (ans2.toUpperCase() === 'TWENTY' || '20'){
+ correct = correct +1; 
+}
+if(ans3.toUpperCase() === 'ALASKA'){
+ correct = correct +1; 
 }
 
 // output results
