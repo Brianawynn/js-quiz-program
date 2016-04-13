4b0e9254dbebeea18317197c1c098a61e389e495
Brianawynn <wynn.briana@wustl.edu>
2016-04-13 18:29:20 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -6,15 +6,15 @@ var correct = 0;
 
 // prompt
 var ans1 = prompt("What do you call a group of cats?");
-document.write("The question was: What do you call a group of cats?  You responded: " + ans1 + " The correct answer was CLOWDER");
+document.write("<p>The question was: What do you call a group of cats?  You responded: " + ans1 + " The correct answer was CLOWDER</p>");
 var ans2 = prompt("How many muscles control a cat's ears?")
-document.write("The question was: How many muscles control a cat's ears?  You responded: " + ans2 + " The correct answer was TWENTY");
+document.write("<p>The question was: How many muscles control a cat's ears?  You responded: " + ans2 + " The correct answer was TWENTY</p>");
 var ans3 = prompt("Which state has elected a cat for mayor of one of its cities?")
-document.write("The question was: Which state has elected a cat for mayor of one of its cities?  You responded: " + ans3 + " The correct answer was Alaska");
+document.write("<p>The question was: Which state has elected a cat for mayor of one of its cities?  You responded: " + ans3 + " The correct answer was Alaska</p>");
 var ans4 = prompt("What flavor can cats NOT taste?")
-document.write("The question was: What flavor can cats NOT taste?  You responded: " + ans4 + " The correct answer was SWEETNESS");
+document.write("<p>The question was: What flavor can cats NOT taste?  You responded: " + ans4 + " The correct answer was SWEETNESS</p>");
 var ans5 = prompt("What is the technical term for a hairball?")
-document.write("The question was: What is the technical term for a hairball?  You responded: " + ans5 +" The correct answer was BEZOAR");
+document.write("<p>The question was: What is the technical term for a hairball?  You responded: " + ans5 +" The correct answer was BEZOAR</p>");
 // if statement
 if ( ans1.toUpperCase() === 'CLOWDER' ) {
  correct = correct+ 1;
@@ -37,11 +37,11 @@ console.log(correct);
 // output results
 if (correct<= 2){
 
-document.write("YOU GOT " + correct + " OUT OF 5 CORRECT. I SUGGEST WORKING ON YOUR KNOWLEDGE!YOU RECIEVE A BRONZE MEDAL. ");
+document.write("<p>YOU GOT " + correct + " OUT OF 5 CORRECT. I SUGGEST WORKING ON YOUR KNOWLEDGE!YOU RECIEVE A BRONZE MEDAL. </p> ");
 }
 if (correct > 2 && correct <= 4){
- document.write("YOU GOT " + correct + " OUT OF 5 CORRECT. I THINK YOUR CAT KNOWLEDGE COULD USE A LITTLE MORE WORK! YOU RECIEVE A SILVER MEDAL!");
+ document.write("<p>YOU GOT " + correct + " OUT OF 5 CORRECT. I THINK YOUR CAT KNOWLEDGE COULD USE A LITTLE MORE WORK! YOU RECIEVE A SILVER MEDAL!</p>");
  if (correct >4){
-  document.write("YOU GOT " + correct + " OUT OF 5 CORRECT. YOU ARE A MASTER IN CAT KNOWLDEGE! YOU RECIEVE A GOLD MEDAL");
+  document.write("<p>YOU GOT " + correct + " OUT OF 5 CORRECT. YOU ARE A MASTER IN CAT KNOWLDEGE! YOU RECIEVE A GOLD MEDAL</p>");
  }
 }
