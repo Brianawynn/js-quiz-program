f2b20cbe16f12cd9050d48309992c5a9737271a1
Brianawynn <wynn.briana@wustl.edu>
2016-04-18 18:08:20 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -58,7 +58,8 @@ document.write("<p>YOU GOT " + correct + " OUT OF " + totalAsked+ " CORRECT. I S
 }
 if (correct > 2 && correct <= 4){
  document.write("<p>YOU GOT " + correct + " OUT OF " + totalAsked + " CORRECT. I THINK YOUR CAT KNOWLEDGE COULD USE A LITTLE MORE WORK! YOU RECIEVE A SILVER MEDAL!</p>");
+}
 if (correct >4){
   document.write("<p>YOU GOT " + correct + " OUT OF " + totalAsked + " CORRECT. YOU ARE A MASTER IN CAT KNOWLDEGE! YOU RECIEVE A GOLD MEDAL</p>");
  }
-}
+
