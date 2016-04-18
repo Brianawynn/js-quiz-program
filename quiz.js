27c642b0dda1dfba23a9afc83cad76f0155c70fa
Brianawynn <wynn.briana@wustl.edu>
2016-04-18 17:55:52 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -5,32 +5,42 @@ document.write("<h2> Become the next master in cat knowledge!</h2> <p> This is f
 var correct = 0;
 
 // prompt
-var ans1 = prompt("What do you call a group of cats?");
-document.write("<p>The question was: What do you call a group of cats?  You responded: " + ans1 + " The correct answer was CLOWDER</p>");
-var ans2 = prompt("How many muscles control a cat's ears?")
-document.write("<p>The question was: How many muscles control a cat's ears?  You responded: " + ans2 + " The correct answer was TWENTY</p>");
-var ans3 = prompt("Which state has elected a cat for mayor of one of its cities?")
-document.write("<p>The question was: Which state has elected a cat for mayor of one of its cities?  You responded: " + ans3 + " The correct answer was Alaska</p>");
-var ans4 = prompt("What flavor can cats NOT taste?")
-document.write("<p>The question was: What flavor can cats NOT taste?  You responded: " + ans4 + " The correct answer was SWEETNESS</p>");
-var ans5 = prompt("What is the technical term for a hairball?")
-document.write("<p>The question was: What is the technical term for a hairball?  You responded: " + ans5 +" The correct answer was BEZOAR</p>");
-// if statement
-if ( ans1.toUpperCase() === 'CLOWDER' ) {
- correct = correct+ 1;
+// var ans1 = prompt("What do you call a group of cats?");
+// document.write("<p>The question was: What do you call a group of cats?  You responded: " + ans1 + " The correct answer was CLOWDER</p>");
+// var ans2 = prompt("How many muscles control a cat's ears?")
+// document.write("<p>The question was: How many muscles control a cat's ears?  You responded: " + ans2 + " The correct answer was TWENTY</p>");
+// var ans3 = prompt("Which state has elected a cat for mayor of one of its cities?")
+// document.write("<p>The question was: Which state has elected a cat for mayor of one of its cities?  You responded: " + ans3 + " The correct answer was Alaska</p>");
+// var ans4 = prompt("What flavor can cats NOT taste?")
+// document.write("<p>The question was: What flavor can cats NOT taste?  You responded: " + ans4 + " The correct answer was SWEETNESS</p>");
+// var ans5 = prompt("What is the technical term for a hairball?")
+// document.write("<p>The question was: What is the technical term for a hairball?  You responded: " + ans5 +" The correct answer was BEZOAR</p>");
+
+//rewrite the above code in a function to simplify it.
+
+function question(){
+ //what our function will do 
+  var ansInput = prompt("What do you call a group of cats?");
+   if ( ansInput.toUpperCase() === 'CLOWDER' ) {
+  correct += 1;
 }
-if (ans2.toUpperCase() === 'TWENTY' || '20'){
- correct = correct +1; 
-}
-if(ans3.toUpperCase() === 'ALASKA'){
- correct = correct +1; 
-}
-if (ans4.toUpperCase() === 'SWEETNESS'){
- correct = correct +1; 
-}
-if (ans5.toUpperCase() === 'BEZOAR'){
- correct = correct +1;
-}
+
+// // if statement
+// if ( ans1.toUpperCase() === 'CLOWDER' ) {
+//  correct = correct+ 1;
+// }
+// if (ans2.toUpperCase() === 'TWENTY' || '20'){
+//  correct = correct +1; 
+// }
+// if(ans3.toUpperCase() === 'ALASKA'){
+//  correct = correct +1; 
+// }
+// if (ans4.toUpperCase() === 'SWEETNESS'){
+//  correct = correct +1; 
+// }
+// if (ans5.toUpperCase() === 'BEZOAR'){
+//  correct = correct +1;
+// }
 
 //console.log(correct);
 
@@ -39,9 +49,9 @@ if (correct<= 2 || correct = 0){
 
 document.write("<p>YOU GOT " + correct + " OUT OF 5 CORRECT. I SUGGEST WORKING ON YOUR KNOWLEDGE!YOU RECIEVE A BRONZE MEDAL. </p> ");
 }
-if (correct > 2 && correct <= 4){
+else if (correct > 2 && correct <= 4){
  document.write("<p>YOU GOT " + correct + " OUT OF 5 CORRECT. I THINK YOUR CAT KNOWLEDGE COULD USE A LITTLE MORE WORK! YOU RECIEVE A SILVER MEDAL!</p>");
- if (correct >4){
+ else if (correct >4){
   document.write("<p>YOU GOT " + correct + " OUT OF 5 CORRECT. YOU ARE A MASTER IN CAT KNOWLDEGE! YOU RECIEVE A GOLD MEDAL</p>");
  }
 }
